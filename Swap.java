7e6dc24b0162f99843080a7e0ea8609a71881e28
tuahlamof <yhy.neverland@gmail.com>
2016-09-30 14:10:58 -0700

    Add Function Buttons
    four buttons added

---
+++ Swap.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Swap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Swap extends StepButton
+{
+    /**
+     * Act - do whatever the Swap wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if (Greenfoot.mousePressed(this)) {
+            this.swap();
+        }
+    }
+    private void swap() {
+        
+    }
+}
