3365a8c69a69d60ef29c827898706978881a5d72
QIANWEN XIE <qianwen.xie.us@gmail.com>
2016-11-06 19:34:43 -0800

    docker hello word

---
+++ Team202/FindBig.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class FindBig here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FindBig extends StepButton
+{
+    /**
+     * Act - do whatever the FindBig wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public FindBig()
+    {
+        GreenfootImage image = getImage() ;
+        image.scale( 250, 80 ) ; 
+    }
+    public void act() 
+    {
+        // Add your action code here.
+        if (Greenfoot.mousePressed(this)) {
+            this.findBig();
+        }
+    }
+    private void findBig() {
+        QuickSortWorld world = (QuickSortWorld)this.getWorld();
+        List<MinionTall> minionTall = world.getObjects(MinionTall.class);
+        int start = world.getStartIndex();
+        int pivot = world.getPivot();
+        int[] list = world.list;
+        int tallIndex = minionTall.get(0).getIndex();
+        while (tallIndex >= start && list[tallIndex] > pivot) {
+            int location = minionTall.get(0).getLocationX();
+            System.out.println("new Location: " + location);
+            location -= 50;
+            System.out.println("new Location: " + location);
+            minionTall.get(0).moveTo(location);
+            minionTall.get(0).setLocationX(location);
+            tallIndex--;
+            minionTall.get(0).setIndex(tallIndex);
+            Greenfoot.delay(20);
+        }
+    }
+}
---
+++ Team202/MinionShort.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MinionShort here.
+ * This is the short minion which pick the lower number in the sequence.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MinionShort extends Minions
+{
+    /**
+     * Act - do whatever the MinionShort wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+        // Add your action code here.
+    public MinionShort()
+    {
+        GreenfootImage image = getImage() ;
+        image.scale( 30, 30 ) ; 
+    }
+
+  
+}
